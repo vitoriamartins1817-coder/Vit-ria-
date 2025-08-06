a1ba85ed87007701fdc7bb17c83c7ea595acdae6
vitoriamartins1817-coder <vitoriamartins.1817@gmail.com>
2025-08-06 11:17:53 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+ nome =  tnput("Digite seu nome:")
+idade = int(input("Digite sua idade: ")
+
+frasedoseudia = input("Digite uma frase do seu dia") 
+A =  int(input(" Digite um número: ")
+B =  int(input(" Digite outro número: ") 
+Soma = A + B 
+
+print(nome, idade, frasedoseudia,)
+print(A,  B, Soma)
